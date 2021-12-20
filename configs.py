f296d77d3d4369c0fa7bbf4c9fbe6bc1792c9f42
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-20 14:47:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,25 +2,29 @@
 
 import os
 
+
 class Config(object):
 	API_ID = int(os.environ.get("API_ID"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	GOFILE_API = os.environ.get("uKR1YsttcRYQHZB5GgTv5Zw3uGyXCIhA")
+	GOFILE_API = os.environ.get("GOFILE_API")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
 	SESSION_NAME = os.environ.get("SESSION_NAME", "CloudManagerBot")
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
 	HELP_TEXT = """
 Send me any Media & Choose Upload Server,
 I will Upload the Media to that server.
 
 Currently I can Upload to:
 > GoFile.io
-> streamtape.com
+> Streamtape.com
+> Pixeldrain.com
 
-For More @HB4All @HB4All_Bot >>>__
+Also I can do a lot of things from Inline!
+__Check Below Buttons >>>__
 """
 	PROGRESS = """
 Percentage : {0}%
